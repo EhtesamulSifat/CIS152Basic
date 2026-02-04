ae472479d14d1edc3a699e92bbe84d91092a6f45
EhtesamulSifat <Sakibhaque94@gmail.com>
2026-02-03 19:51:34 -0500

    Added Buttons and applied shortcuts

--- Sifat/Form1.Designer.cs
+++ Sifat/Form1.Designer.cs
@@ -32,52 +32,110 @@
             this.label2 = new System.Windows.Forms.Label();
             this.txtDogOwner = new System.Windows.Forms.TextBox();
             this.btnCalc = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnQuit = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(145, 9);
+            this.label1.Location = new System.Drawing.Point(290, 17);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(263, 20);
+            this.label1.Size = new System.Drawing.Size(504, 37);
             this.label1.TabIndex = 0;
             this.label1.Text = "Dog Walker Weekly Transaction";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(25, 55);
+            this.label2.Location = new System.Drawing.Point(58, 103);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.Size = new System.Drawing.Size(119, 25);
             this.label2.TabIndex = 1;
             this.label2.Text = "Dog Owner";
             // 
             // txtDogOwner
             // 
-            this.txtDogOwner.Location = new System.Drawing.Point(149, 52);
+            this.txtDogOwner.Location = new System.Drawing.Point(214, 100);
+            this.txtDogOwner.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.txtDogOwner.Name = "txtDogOwner";
-            this.txtDogOwner.Size = new System.Drawing.Size(246, 20);
+            this.txtDogOwner.Size = new System.Drawing.Size(604, 31);
             this.txtDogOwner.TabIndex = 2;
             // 
             // btnCalc
             // 
-            this.btnCalc.Location = new System.Drawing.Point(28, 300);
+            this.btnCalc.Location = new System.Drawing.Point(70, 553);
+            this.btnCalc.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.btnCalc.Name = "btnCalc";
-            this.btnCalc.Size = new System.Drawing.Size(75, 23);
+            this.btnCalc.Size = new System.Drawing.Size(150, 48);
             this.btnCalc.TabIndex = 3;
-            this.btnCalc.Text = "Calculate";
+            this.btnCalc.Text = "&Calculate";
             this.btnCalc.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(58, 182);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(280, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Number Of Walks Per Week";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(344, 176);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(474, 31);
+            this.textBox1.TabIndex = 5;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(70, 255);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(785, 204);
+            this.listBox1.TabIndex = 6;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(407, 553);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(112, 44);
+            this.btnReset.TabIndex = 7;
+            this.btnReset.Text = "&Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.Location = new System.Drawing.Point(747, 557);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(99, 44);
+            this.btnQuit.TabIndex = 8;
+            this.btnQuit.Text = "&Quit";
+            this.btnQuit.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(553, 360);
+            this.ClientSize = new System.Drawing.Size(1106, 651);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.btnCalc);
             this.Controls.Add(this.txtDogOwner);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "Form1";
             this.Text = "Sifat\'s Dog walking Service";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -92,6 +150,11 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtDogOwner;
         private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnQuit;
     }
 }
 
